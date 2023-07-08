479d22fce76b25274fb4e2f659366c7c1e9d8d28
SyRachid <syrachidsanogo78@gmail.com>
2023-07-08 13:49:49 +0000

    init of the interface

---
+++ lib/main.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  home:Home()
+));
+class Home extends StatelessWidget{
+  @override
+  Widget build(BuildContext context)
+  {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("EMOS X-Transfer",
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 20.0,
+              )),
+          centerTitle: true,
+          backgroundColor: Colors.red[600],
+        ),
+        body: Center(
+            child:Text("authentification",
+                style: TextStyle(
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                  letterSpacing: 2.0,
+                  color:Colors.grey[600],
+                )
+            )
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: (){},
+          child: Text("click   "),
+          backgroundColor: Colors.red[600],
+        )
+    ) ;
+  }
+}
